0fa89a94c05001b60785cb0f1fd36e70030381d3
Li, Diyang <diyangli233@gmail.com>
2022-07-11 22:21:06 -0400

    upload file to page

--- src/main/java/com/itheima/reggie/controller/CommonController.java
+++ src/main/java/com/itheima/reggie/controller/CommonController.java
@@ -3,13 +3,15 @@ package com.itheima.reggie.controller;
 import com.itheima.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.UUID;
 import java.util.logging.Filter;
 
@@ -52,5 +54,30 @@ public class CommonController {
             e.printStackTrace();
         }
         return R.success(fileName);
+    }
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            // set the file type is image/jpeg
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((len = fileInputStream.read(bytes)) != -1){
+                outputStream.write(bytes, 0, len);
+                outputStream.flush();
+            }
+
+            // close resource
+            outputStream.close();
+            fileInputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
